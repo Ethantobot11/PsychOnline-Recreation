ad7751cd1a991777bdc8b57585f4cc79e10e658c
Ethantobot11 <156458071+Ethantobot11@users.noreply.github.com>
2025-07-24 16:55:10 +0200

    unzipgit

--- source/backend/CoolUtil.hx
+++ source/backend/CoolUtil.hx
@@ -12,9 +12,6 @@ import sys.io.File;
 import sys.FileSystem;
 #end
 
-#if cpp
-@:cppFileCode('#include <thread>')
-#end
 class CoolUtil
 {
 	inline public static function quantize(f:Float, snap:Float){
@@ -148,23 +145,4 @@ class CoolUtil
 		}
 		return haxArr;
 	}
-
-	public static function showPopUp(message:String, title:String):Void
-	{
-		/*#if android
-		AndroidTools.showAlertDialog(title, message, {name: "OK", func: null}, null);
-		#else*/
-		FlxG.stage.window.alert(message, title);
-		//#end
-	}
-
-	#if cpp
-    @:functionCode('
-        return std::thread::hardware_concurrency();
-    ')
-	#end
-    public static function getCPUThreadsCount():Int
-    {
-        return 1;
-    }
 }
