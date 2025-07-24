1c8a258bb79d72877ac39840441c64a61e5ffab5
Ethantobot11 <156458071+Ethantobot11@users.noreply.github.com>
2025-07-24 16:54:23 +0200

    unzipgit

--- source/online/states/ResultsState.hx
+++ source/online/states/ResultsState.hx
@@ -2,12 +2,13 @@ package online.states;
 
 import flixel.util.FlxStringUtil;
 import backend.WeekData;
-import lumod.Lumod;
 import flixel.effects.FlxFlicker;
 import sys.FileSystem;
 import objects.Character;
 
+#if lumod
 @:build(lumod.LuaScriptClass.build())
+#end
 class ResultsState extends MusicBeatState {
 	public static var gainedPoints:Float = 0;
 
@@ -57,8 +58,10 @@ class ResultsState extends MusicBeatState {
         
         FlxG.sound.music.stop();
 
+		#if lumod
 		if (luaValue == false)
 			return;
+		#end
 
 		FlxG.sound.playMusic(Paths.music('title'), 0);
 		FlxG.sound.music.onComplete = () -> {
@@ -392,7 +395,6 @@ class ResultsState extends MusicBeatState {
 		});
 
 		registerMessages();
-		addTouchPad('NONE', 'B_C_T');
     }
 
 	function registerMessages() {
@@ -424,6 +426,7 @@ class ResultsState extends MusicBeatState {
 	override function update(elapsed) {
         super.update(elapsed);
 
+		#if lumod
 		if (FlxG.keys.justPressed.F12) {
 			trace('reloading lumod');
 			Lumod.cache.scripts.clear();
@@ -432,6 +435,8 @@ class ResultsState extends MusicBeatState {
 
 		if (luaValue == false)
 			return;
+
+		#end
 
 		if (!disableInput) {
 			if (back.animation.curAnim.name != "press")
