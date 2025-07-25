0190828badca164df9fc495327b24ee5053aea33
Ethantobot11 <156458071+Ethantobot11@users.noreply.github.com>
2025-07-25 14:46:32 +0200

    unzipgit

--- source/online/gui/DownloadAlert.hx
+++ source/online/gui/DownloadAlert.hx
@@ -53,7 +53,7 @@ class DownloadAlerts extends Sprite {
 					alert.cancelText.text = 'Cancelling...';
 				}
 				else {
-					alert.cancelText.text = 'Cancel: ALT + $i ';
+					alert.cancelText.text = 'Cancel: ${Controls.instance.mobileC ? 'Touch Here' : 'ALT + $i'} ';
 					if (i >= 10) {
 						alert.cancelText.text = "";
 					}
@@ -69,7 +69,7 @@ class DownloadAlerts extends Sprite {
 					case FAILED(exc):
 						alert.setStatus("Failed! " + exc);
 					case DOWNLOADED:
-						alert.setStatus("Preparing to instal...");
+						alert.setStatus("Preparing to install...");
 					case INSTALLING:
 						alert.setStatus("Installing...");
 					case FINISHED:
@@ -102,6 +102,9 @@ class DownloadAlerts extends Sprite {
 
 			alert.cancelBg.scaleX = alert.cancelText.textWidth;
 			alert.cancelBg.scaleY = alert.cancelText.textHeight + 5;
+
+			if (Controls.instance.mobileC && alert.cancelBg.getBounds(FlxG.stage).contains(FlxG.stage.mouseX, FlxG.stage.mouseY) && FlxG.mouse.justPressed)
+				downloader.client.cancel();
 
 			prevAlert = alert;
 			i++;
