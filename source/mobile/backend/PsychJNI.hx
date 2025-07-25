79624ed19477a1ba83e2745b033199500f849101
Ethantobot11 <156458071+Ethantobot11@users.noreply.github.com>
2025-07-25 14:45:01 +0200

    unzipgit

--- source/mobile/backend/PsychJNI.hx
+++ source/mobile/backend/PsychJNI.hx
@@ -1,25 +1,3 @@
-/*
- * Copyright (C) 2025 Mobile Porting Team
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- */
-
 package mobile.backend;
 
 /**
@@ -40,10 +18,8 @@ class PsychJNI #if (lime >= "8.0.0") implements JNISafety #end
 	public static inline function setOrientation(width:Int, height:Int, resizeable:Bool, hint:String):Dynamic
 		return setOrientation_jni(width, height, resizeable, hint);
 
-	public static inline function getCurrentOrientationAsString():String
-	{
-		return switch (getCurrentOrientation_jni())
-		{
+	public static inline function getCurrentOrientationAsString():String {
+		return switch (getCurrentOrientation_jni()) {
 			case SDL_ORIENTATION_PORTRAIT: "Portrait";
 			case SDL_ORIENTATION_LANDSCAPE: "LandscapeRight";
 			case SDL_ORIENTATION_PORTRAIT_FLIPPED: "PortraitUpsideDown";
