204c1a581a7e94a77d1657b2de70d63b62cc0999
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-08-01 22:42:52 +0200

    unzipgit

--- source/objects/HealthIcon.hx
+++ source/objects/HealthIcon.hx
@@ -4,8 +4,9 @@ class HealthIcon extends FlxSprite
 {
 	public var sprTracker:FlxSprite;
 	private var isOldIcon:Bool = false;
-	private var isPlayer:Bool = false;
+	public var isPlayer:Bool = false;
 	private var char:String = '';
+	public var ox:Int;
 
 	public function new(char:String = 'bf', isPlayer:Bool = false, ?allowGPU:Bool = true)
 	{
@@ -28,9 +29,9 @@ class HealthIcon extends FlxSprite
 	public function changeIcon(char:String, ?allowGPU:Bool = true) {
 		if(this.char != char) {
 			var name:String = 'icons/' + char;
-			if(!Paths.fileExists('images/' + name + '.png', IMAGE) && !Paths.fileExists('images/' + name + '.astc', BINARY)) name = 'icons/icon-' + char; //Older versions of psych engine's support
-			if(!Paths.fileExists('images/' + name + '.png', IMAGE) && !Paths.fileExists('images/' + name + '.astc', BINARY)) name = 'icons/icon-face'; //Prevents crash from missing icon
-
+			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
+			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
+			
 			var graphic = Paths.image(name, allowGPU);
 			var iSize:Float = Math.round(graphic.width / graphic.height);
 			loadGraphic(graphic, true, Math.floor(graphic.width / iSize), Math.floor(graphic.height));
