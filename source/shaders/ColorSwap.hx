f6f6790e0602106dba64d9c0aaa197f6bf6986ee
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-08-01 22:43:41 +0200

    unzipgit

--- source/shaders/ColorSwap.hx
+++ source/shaders/ColorSwap.hx
@@ -1,6 +1,6 @@
 package shaders;
 
-import shaders.flixel.system.FlxShader;
+import flixel.system.FlxAssets.FlxShader;
 
 class ColorSwap {
 	public var shader(default, null):ColorSwapShader = new ColorSwapShader();
@@ -46,15 +46,10 @@ class ColorSwapShader extends FlxShader {
 
 		uniform bool hasTransform;
 		uniform bool hasColorTransform;
-		uniform bool isTexture;
 
 		vec4 flixel_texture2D(sampler2D bitmap, vec2 coord)
 		{
 			vec4 color = texture2D(bitmap, coord);
-
-			if (isTexture)
-				color.rgb *= color.a;
-
 			if (!hasTransform)
 			{
 				return color;
